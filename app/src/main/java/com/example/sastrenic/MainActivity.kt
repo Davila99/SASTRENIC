ac221a3353760207a0fc16c26740309f1caf90ff
Davila99 <davilaeliseo453@gmail.com>
2021-09-02 10:35:13 -0600

    Hemos edtitado algunas funciones en el login con el usuario eliseo y la contraseña DAVILA de forma quemada dentro de un if en la clase MainActivity

--- app/src/main/java/com/example/sastrenic/MainActivity.kt
+++ app/src/main/java/com/example/sastrenic/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.sastrenic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun startAplication(view: View) {
+        var usuario = findViewById<EditText>(R.id.editUsuario).text.toString();
+        var contraseña = findViewById<EditText>(R.id.editContraseña).text.toString();
+        if (usuario == "eliseo" && contraseña == "1234") {
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
