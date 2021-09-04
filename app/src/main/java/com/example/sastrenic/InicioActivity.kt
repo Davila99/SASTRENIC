ecd803f70db0f276d522fdf8c0c7fa3f50c790b2
Davila99 <davilaeliseo453@gmail.com>
2021-09-04 11:34:58 -0600

    Aqui creamos una lista de clientes vacias y declaramos un variable para llamar a nuestra base de datos, utilzando el adaptador  para listar los clientes

--- app/src/main/java/com/example/sastrenic/InicioActivity.kt
+++ app/src/main/java/com/example/sastrenic/InicioActivity.kt
@@ -1,11 +1,46 @@
 package com.example.sastrenic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_inicio.*
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+        inicio();
     }
+
+    fun inicio() {
+
+        var listaclientes = emptyList<Cliente>()
+
+        val database = AppDatabase.getDatabase(this)
+
+        database.clientes().getAll().observe(this, Observer {
+            listaclientes = it
+
+            val adapter = ClienteAdapter(this, listaclientes)
+
+            lista.adapter = adapter
+
+        })
+
+
+
+        lista.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, ClienteActivity::class.java)
+            intent.putExtra("id", listaclientes[position].idCliente )
+            startActivity(intent)
+        }
+        //esta nombre el la refecia de evento onclick en el boton guardar
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this, NuevoClienteActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
 }
