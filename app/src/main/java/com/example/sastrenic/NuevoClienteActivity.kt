661d5b81e29829b386969cadfbdfd1e2854d84f6
Davila99 <davilaeliseo453@gmail.com>
2021-09-04 11:48:13 -0600

    Aqui programos el boton save con su respectivo evento

--- app/src/main/java/com/example/sastrenic/NuevoClienteActivity.kt
+++ app/src/main/java/com/example/sastrenic/NuevoClienteActivity.kt
@@ -2,10 +2,63 @@ package com.example.sastrenic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_nuevo_cliente.*
+import kotlinx.android.synthetic.main.cliente_list.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NuevoClienteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_cliente)
+
+        var idCliente: Int? = null
+
+        if (intent.hasExtra("cliente")) {
+
+            val cliente = intent.extras?.getSerializable("cliente") as Cliente
+
+            et_nombre.setText(cliente.nombre)
+            et_cedula.setText(cliente.cedula)
+            et_telefono.setText(cliente.telefono.toString())
+            et_fecha.setText(cliente.fecha_recepcion)
+            idCliente = cliente.idCliente
+        }
+
+        val database = AppDatabase.getDatabase(this)
+
+        btn_guadar.setOnClickListener {
+
+            val nombre = et_nombre.text.toString()
+            val cedula = et_cedula.text.toString()
+            val telefono = et_telefono.text.toString().toInt()
+            val fecha = et_fecha.text.toString()
+
+            val cliente = Cliente(nombre, cedula, telefono, fecha)
+
+            if (idCliente != null) {
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    cliente.idCliente = idCliente
+
+                    database.clientes().update(cliente)
+
+                    this@NuevoClienteActivity.finish()
+                }
+            } else {
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    database.clientes().insertAll(cliente)
+
+                    this@NuevoClienteActivity.finish()
+                }
+
+
+            }
+
+        }
     }
 }
